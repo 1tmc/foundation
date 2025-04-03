a4dd218cde860228bf76426c51beda06225f3a0a
1tmc <158778648+1tmc@users.noreply.github.com>
2025-04-03 12:40:57 +0000

    Update index.tsx

--- src/components/inner-pages/volunteers/index.tsx
+++ src/components/inner-pages/volunteers/index.tsx
@@ -2,7 +2,6 @@ import Breadcrumb from "@/components/common/Breadcrumb"
 import FooterOne from "@/layout/footers/FooterOne"
 import HeaderOne from "@/layout/headers/HeaderOne"
 import Volunteer from "@/components/homes/home-one/Volunteer"
-import VolunteerTwo from "@/components/homes/home-two/Volunteer"
 import VolunteerThree from "@/components/homes/home-three/Volunteer"
 
 const Volunteers = () => {
@@ -12,7 +11,6 @@ const Volunteers = () => {
          <main>
             <Breadcrumb page_title="Our Volinteers" page_list="Volinteers" style={false} />
             <Volunteer style={false} />
-            <VolunteerTwo style={false} />
             <VolunteerThree style={false} />
          </main>
          <FooterOne />
